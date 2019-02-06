b511a6f2de244f1f7b391d03437f22c5226439d8
Elaina <elainaseki@gmail.com>
2019-02-06 01:05:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(string)
  if string == string.upcase
     return "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA"
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
     return"HUH?! SPEAK UP, SONNY!"
  end
